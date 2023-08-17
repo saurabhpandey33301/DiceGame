4dd13216e17ace6bca95c271e9cf4c641e3ac236
saurabhpandey33301 <124060265+saurabhpandey33301@users.noreply.github.com>
2023-08-17 16:02:58 +0530

    bd

--- diceGames.js
+++ diceGames.js
@@ -16,16 +16,16 @@ function DiceRoll(){
       let diceNum1 = document.querySelector(".img1");
       let diceNum2 = document.querySelector(".img2");
 
-      diceNum1.setAttribute("src","image/dice.gif");
-      diceNum2.setAttribute("src","image/dice.gif");
+      diceNum1.setAttribute("src","dice.gif");
+      diceNum2.setAttribute("src","dice.gif");
 
       let result = document.querySelector('h1');
       setTimeout( () => {
         let randomNum1 = Math.floor(Math.random()*6)+1;
         let randomNum2 = Math.floor(Math.random()*6)+1;
         
-        diceNum1.setAttribute("src",'image/dice'+randomNum1+'.png');
-        diceNum2.setAttribute("src",'image/dice'+randomNum2+'.png');
+        diceNum1.setAttribute("src",'dice'+randomNum1+'.png');
+        diceNum2.setAttribute("src",'dice'+randomNum2+'.png');
         
         if(randomNum1==randomNum2){
             result.innerHTML = "Draw!";
